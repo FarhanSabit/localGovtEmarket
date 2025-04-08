b0048f14416b27437f47a941aae70faf8502d533
Mamun-Miah <mahadyhasen7@gmail.com>
2025-04-08 15:24:36 +0600

    Market Level Shop List Updated

--- controllers/marketLevelshoplist.js
+++ controllers/marketLevelshoplist.js
@@ -1,24 +1,49 @@
 const db = require('../db/db');
 const jwt = require('jsonwebtoken');
+
+
 exports.getMarketLevelShopList = async (req, res) => {
     const token = req.session.token;
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     const city_id = decoded.city_id;
     const user_role =decoded.user_role;
-    
+    const market_id = decoded.market_id;
+
+    const marketIds = req.session.marketIds;// Get marketId from query select market dropdown
     // console.log(`City ID From market Level ${city_id}`)
+    try {
+        if (user_role === 'admin') {
 
-    if (user_role === 'admin') {
-        const MarketLevelShopListData = await new Promise((resolve, reject) => {
-            db.query('SELECT * FROM customer WHERE city_id = ?', [city_id], (err, results) => {
-                if (err) reject(err);
-                resolve(results);
+            if (marketIds){
+                const MarketLevelShopListData = await new Promise((resolve, reject) => {
+                    db.query('SELECT * FROM customer WHERE market_id = ?', [marketIds], (err, results) => {
+                        if (err) reject(err);
+                        resolve(results);
+                    });
+                });
+               return res.render('MarketLevelShopList',{ MarketLevelShopListData });
+            }
+            // pass Market Level Shop List by City_id Admin
+            const MarketLevelShopListData = await new Promise((resolve, reject) => {
+                db.query('SELECT * FROM customer WHERE city_id = ?', [city_id], (err, results) => {
+                    if (err) reject(err);
+                    resolve(results);
+                });
             });
-        });
-        res.render('MarketLevelShopList',{ MarketLevelShopListData });
+            res.render('MarketLevelShopList',{ MarketLevelShopListData });
+        } else {
+            const MarketLevelShopListData = await new Promise((resolve, reject) => {
+                db.query('SELECT * FROM customer WHERE market_id = ?', [market_id], (err, results) => {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            });
+            res.render('MarketLevelShopList',{ MarketLevelShopListData });
+        }
+    } 
+    catch (error) {
+        console.error('Error fetching market data:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
-    
-    
-
     
 };
