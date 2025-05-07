017fff20c21deac5f69bbaf7aa0d5127f4a54c98
Mamun-Miah <mahadyhasen7@gmail.com>
2025-05-07 17:50:49 +0600

    city admin User can update delete edit market member

--- controllers/shopRents.js
+++ controllers/shopRents.js
@@ -1,4 +1,78 @@
 const db = require('../db/db');
 
-exports.shopRents = (req, res) => {res.render('shopRents');};
-exports.addshopRents = (req, res) => {res.render('addshopRents');};
+
+//Render Shop Rents
+exports.renderShopRentsPage = async (req, res) => {
+    const { city_id, user_role } = req.user;
+    const marketIds = req.session.marketIds; //market id from ajax & its set to req.session from footer js
+
+    try {
+    if (user_role === 'admin') {
+        
+        if (marketIds) {
+            const loadShopRentsData = await new Promise((resolve, reject) => {
+
+                db.query('SELECT * FROM customer WHERE market_id = ?', [marketIds], (err, results) => {
+                    if (err) reject(err);
+                    resolve(results);
+                });
+            });
+            
+            res.render('shopRents', {
+                loadShopRentsDatas: loadShopRentsData,
+            });
+        } else {
+            const loadShopRentsData = await new Promise((resolve, reject) => {
+
+                db.query('SELECT * FROM customer WHERE city_id = ?', [city_id], (err, results) => {
+                    if (err) reject(err);
+                    resolve(results);
+                   
+                });
+            });
+            res.render('shopRents', {
+                loadShopRentsDatas: loadShopRentsData,
+            });
+        }
+
+        
+        
+        
+    }
+    
+    }catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).send('Error fetching users');
+    }
+};
+
+
+
+
+// exports.addshopRents = (req, res) => {res.render('addshopRents');};
+
+
+//add shop rents 
+exports.addShopRents = async (req, res) => {
+    // const { city_id, user_role } = req.user;
+    // const marketIds = req.session.marketIds; //market id from ajax & its set to req.session from footer js
+    
+    try {
+        const { name, fc_no, mth_pay} = req.body;
+
+        await new Promise((resolve, reject) => {
+            db.query('INSERT INTO customer SET ?', { name, fc_no, mth_pay }, (err) => {
+                if (err) reject(err);
+                resolve();
+            });
+        });
+        // res.redirect('/shopRents');
+    }catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).send('Error fetching users');
+    }
+
+
+
+  
+};
--- routes/shopRents.js
+++ routes/shopRents.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const shopRents = require('../controllers/shopRents.js');
-// const dummyController = require('../controllers/dummyController.js');
 const authenticateToken = require('../Middleware/authenticateToken.js');
- 
+
 
 // router.get('/showshoprents', authenticateToken, shopRents.showshoprents);
 // router.post('/get-market-data:id',authenticateToken, shopRents.postMarketData);
-router.get('/shopRents', authenticateToken, shopRents.shopRents); 
-router.get('/addshopRents', authenticateToken, shopRents.addshopRents);     
+router.get('/shopRents', authenticateToken, shopRents.renderShopRentsPage); 
+// router.get('/addshopRents', authenticateToken, shopRents.addshopRents);
+
+router.post('/add-shop-rents', authenticateToken, shopRents.addShopRents)
 
 module.exports = router;
